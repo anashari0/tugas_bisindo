282a1ec08dee857955517d342ab621c266a85666
anashari0 <an.ashari@outlook.com>
2026-06-26 02:45:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -37,8 +37,7 @@ st.markdown("""
 st.divider()
 
 st.subheader("🤖 Tutor AI BISINDO")
-st.write("Tanyakan kata, kalimat, atau etika berinteraksi dengan teman Tuli. <br>
-AI akan membantu menjelaskannya untuk Anda.")
+st.write("Tanyakan kata, kalimat, atau etika berinteraksi dengan teman Tuli. AI akan membantu menjelaskannya untuk Anda.")
 
 user_input = st.text_input("Ketik di sini (contoh: bagaimana cara isyarat 'terima kasih'?):")
 
